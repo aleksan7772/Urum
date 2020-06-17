99fe13dec27c1fd14621521709726284cc306266
Serg Shaxov <maksim155555@gmail.com>
2020-06-17 18:39:38 +0300

    2.0

--- src/lab21/Test.java
+++ src/lab21/Test.java
@@ -1,15 +1,17 @@
 package lab21;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         List<String> list1 = new ArrayList<>();
         list1 = Arrays.asList("a", "b", "c");
         List<String> list2 = new ArrayList<>(list1);
-        List<String> list3 = Arrays.asList();
-
+        List<String> list3 =  Arrays.asList("L", "I", "S","T3");
+        list2.add(String.valueOf(list3));
+        System.out.println(list2);
+        list2.sort();
     }
 }
